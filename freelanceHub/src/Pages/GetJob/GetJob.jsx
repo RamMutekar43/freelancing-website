54fbd2bee181df82aa0c92515928afea205a45e0
RamMutekar43 <152324887+RamMutekar43@users.noreply.github.com>
2025-03-27 23:45:09 +0530

    commit

--- freelanceHub/src/Pages/GetJob/GetJob.jsx
+++ freelanceHub/src/Pages/GetJob/GetJob.jsx
@@ -1,22 +1,33 @@
 import JobMenu from "../../components/Jobs/JobMenu"
 import Job from "../../components/Jobs/Job"
 import { Flex, Text, VStack, Box, Button } from '@chakra-ui/react'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
+
+// https://dummyjson.com/c/3616-bd03-43c7-8e7d
 const GetJob = () => {
+  const [isJobs, setIsJobs] = useState([]);
+
+  useEffect(()=>{
+    const getJob = async ()=>{
+      const response = await fetch("https://dummyjson.com/c/3616-bd03-43c7-8e7d")
+      const data = await response.json();
+      console.log(data);
+      setIsJobs(data.jobs);
+    }
+    getJob();
+  },[])
   return (
     <>
-    
     <Flex backgroundColor={'whiteAlpha.500'} my={5} borderRadius={15} py={5} ps={7} pe={8}>
         <VStack me={20} >
-            <Job/>
-            <Job/>
-            <Job/>
-            <Job/>
-            <Job/>
-            <Job/>
-            <Job/>
-            <Job/>
+            {isJobs.map((e,key)=>{
+              return(
+                <>
+                <Job title={e.title} description={e.description} budget={e.budget} key={key}/>
+                </>
+              )
+            })}
         </VStack>
 
         <Box mt={5} position={"fixed"} right={6}>
--- freelanceHub/src/components/Jobs/Job.jsx
+++ freelanceHub/src/components/Jobs/Job.jsx
@@ -2,27 +2,22 @@ import React from 'react'
 import { Flex, Text, VStack, Box, Button } from '@chakra-ui/react'
 import JobDescription from './JobDescription'
 
-const Job = () => {
+const Job = ({title, description,budget}) => {
   return (
     <>
-    <Box pe={10} py={5}>
+    <Box pe={10} py={5} w={'full'}>
     <Flex backgroundColor={'whiteAlpha.400'} w={'full'} shadow={'sm'} borderRadius={5}>
                 <VStack w={'4/5'} py={5} px={10} >
                     <Flex color={'black'} w={'full'} gap={2} alignItems={'baseline'}>
                     <Text textStyle={'xl'} fontWeight={'bold'}>Job Title : </Text>
-                    <Text>Web Development</Text>
+                    <Text>{title}</Text>
                     </Flex>
     
                     <Box color={'black'} w={'full'} h={'20vh'} >
                     <Text textStyle={'xl'} fontWeight={'bold'}>Job Description : </Text>
                     <Box overflowY={'hidden'} w={'full'} h={'10vh'}>
                     <Text>
-                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni eius qui minus quam nesciunt dolor dolore magnam voluptatibus corrupti repellat. Laboriosam facilis a fugit illum accusamus necessitatibus maiores voluptatibus esse.
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi quisquam ea non labore eligendi beatae quas, nobis provident ullam, assumenda perferendis mollitia omnis, est repudiandae. Ratione suscipit animi adipisci magnam.
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis rem ipsam quaerat cumque et amet culpa rerum! Nostrum est laboriosam molestiae unde fugit omnis doloribus saepe libero cupiditate, aliquam officia.
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit qui ducimus quas deleniti ad, atque officiis. Et dolorem necessitatibus voluptatum neque soluta nemo suscipit, mollitia quibusdam doloribus molestias, est temporibus?
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint ex ut blanditiis ad voluptates tenetur, pariatur omnis facere ab odit expedita quam dolorem ullam sed minus incidunt quae ipsum accusantium.
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum sint sequi, dolorum perspiciatis culpa quo assumenda excepturi consequuntur ut accusantium repudiandae quis doloribus id recusandae laborum quam debitis sapiente enim!
+                        {description}
                     </Text>
                     </Box>
                     <JobDescription/>
@@ -30,7 +25,7 @@ const Job = () => {
 
                     <Flex color={'black'} w={'full'} gap={2} alignItems={'baseline'}>
                     <Text textStyle={'xl'} fontWeight={'bold'}>Budget : </Text>
-                    <Text>10K</Text>
+                    <Text>{budget}</Text>
                     </Flex>
                     
                 </VStack>
