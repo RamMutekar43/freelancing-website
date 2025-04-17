dd41aad7c6df2038873eaf8990d59b55714ba485
RamMutekar43 <152324887+RamMutekar43@users.noreply.github.com>
2025-04-17 09:46:46 +0530

    commit

--- freelanceHub/src/App.jsx
+++ freelanceHub/src/App.jsx
@@ -6,9 +6,44 @@ import GetJob from "./Pages/GetJob/GetJob"
 import JobApplication from "./Pages/GetJob/JobApplication"
 import HireJob from "./Pages/HireJob/HireJob"
 import FreelancerProfile from "./Pages/FreelancerProfile/FreelancerProfile"
+import { useEffect, useState } from "react"
+import { couch } from "globals"
+import axios from "axios"
 
 
 function App() {
+  const [users,setUsers] = useState(null);
+
+  // useEffect(()=>{
+  //   const fetchUsers = async ()=>{
+  //     try {
+  //       const response = await fetch('http://localhost:8080/api/users/getAllUsers', {
+  //         method: 'GET',
+  //         headers: {
+  //           'Content-Type': 'application/json',
+  //         },
+  //       });
+  
+  //       if (!response.ok) {
+  //         throw new Error(`HTTP error! Status: ${response.status}`);
+  //       }
+  
+  //       const data = await response.json();
+  //       console.log('Fetched users:', data);
+  //       setUsers(data);
+  //     } catch (error) {
+  //       console.error(error);
+  //     }
+  //   }
+
+  //   fetchUsers()
+  // },[])
+
+  // useEffect(() => {
+  //   axios.get("http://localhost:8080/api/users/getAllUsers")
+  //     .then(res => setUsers(res.data))
+  //     .catch(err => console.error(err));
+  // }, []);
 
   return (
     <>
--- freelanceHub/src/components/AuthForm/Login.jsx
+++ freelanceHub/src/components/AuthForm/Login.jsx
@@ -1,12 +1,47 @@
 import React ,{useState} from 'react'
 import { Input, Button } from '@chakra-ui/react'
+import useAuthStore from '../../Store/authStore'
 import { Alert } from '@chakra-ui/react'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+
 
 const Login = () => {
     const [inputs, setInputs] = useState({
         email:'',
         password:'',
     })
+
+    const loginUser = useAuthStore(state => state.login)
+    const navigate = useNavigate()
+
+    const handleLogin = async () => {
+      try {
+        const res = await fetch('http://localhost:8080/api/auth/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(inputs),
+        });
+  
+        if (res.ok) {
+          const data = await res.json();
+          localStorage.setItem('token',data.token)
+          // saveToken(data.token);
+          // setIsAuthenticated(true);
+          loginUser(res)
+          navigate('/');
+        } else {
+          const err = await res.text();
+          alert(err || 'Login failed');
+        }
+      } catch (error) {
+        alert('Network error: ' + error.message);
+      }
+
+
+    };
 
   return (
     <>
@@ -21,7 +56,7 @@ const Login = () => {
     onChange={(e)=>setInputs({...inputs,password:e.target.value})}
     />
     
-    <Button bg={"blue.500"} w={"full"} fontSize={"sm"} >
+    <Button bg={"blue.500"} w={"full"} fontSize={"sm"} onClick={handleLogin}>
         Log in
     </Button>
     </>
