3e824a5c0ab439b44adddaa589fde4d0fdbe595c
RamMutekar43 <152324887+RamMutekar43@users.noreply.github.com>
2025-04-20 07:04:11 +0530

    commit

--- freelanceHub/src/App.jsx
+++ freelanceHub/src/App.jsx
@@ -1,56 +1,39 @@
 import HomePage from "./Pages/HomePage/HomePage"
-import AuthPage from "./Pages/AuthPage/AuthPage"
 import PageLayout from "./Layouts/PageLayout/PageLayout"
 import { Route, Routes } from "react-router-dom"
 import GetJob from "./Pages/GetJob/GetJob"
 import JobApplication from "./Pages/GetJob/JobApplication"
 import HireJob from "./Pages/HireJob/HireJob"
 import FreelancerProfile from "./Pages/FreelancerProfile/FreelancerProfile"
+import LoginPage from  './Pages/AuthPage/LoginPage'
+import SignupPage from  './Pages/AuthPage/SignupPage'
 import { useEffect, useState } from "react"
 import { couch } from "globals"
 import axios from "axios"
+import { useAuth } from "./Context/AuthContext"
+import { useSelector } from "react-redux"
+
 
 
 function App() {
-  const [users,setUsers] = useState(null);
+  const { token } = useSelector((state)=>state.auth);
+  // const {token} = useAuth()
 
-  // useEffect(()=>{
-  //   const fetchUsers = async ()=>{
-  //     try {
-  //       const response = await fetch('http://localhost:8080/api/users/getAllUsers', {
-  //         method: 'GET',
-  //         headers: {
-  //           'Content-Type': 'application/json',
-  //         },
-  //       });
-  
-  //       if (!response.ok) {
-  //         throw new Error(`HTTP error! Status: ${response.status}`);
-  //       }
-  
-  //       const data = await response.json();
-  //       console.log('Fetched users:', data);
-  //       setUsers(data);
-  //     } catch (error) {
-  //       console.error(error);
-  //     }
-  //   }
+  useEffect(()=>{
+    if(token){
+      localStorage.setItem("user",true)
+      console.log(token)
+    }
+  },[token])
 
-  //   fetchUsers()
-  // },[])
-
-  // useEffect(() => {
-  //   axios.get("http://localhost:8080/api/users/getAllUsers")
-  //     .then(res => setUsers(res.data))
-  //     .catch(err => console.error(err));
-  // }, []);
 
   return (
     <>
     <PageLayout>
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/auth" element={<AuthPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
       <Route path="/get-job" element={<GetJob />} />
       <Route path="/get-job/apply" element={<JobApplication />} />
       <Route path="/hire-job" element={<HireJob />} />
--- freelanceHub/src/main.jsx
+++ freelanceHub/src/main.jsx
@@ -4,16 +4,23 @@ import './index.css'
 import App from './App.jsx'
 import { Provider } from './components/ui/provider'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider as ReduxProvider } from 'react-redux'
+import { store } from './Store/store'
+import { AuthProvider } from './Context/AuthContext'
 // import { JobProvider } from './Context/JobContext'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <JobProvider> */}
+    
     <BrowserRouter>
+    <ReduxProvider store={store}>
+    <AuthProvider>
     <Provider>
     <App />
     </Provider>
+    </AuthProvider>
+    </ReduxProvider>
     </BrowserRouter>
-    {/* </JobProvider> */}
+    
   </StrictMode>,
 )
